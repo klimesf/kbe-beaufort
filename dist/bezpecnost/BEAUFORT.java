98f3a6cbf3da8284bd26c54ecf7c7f9b20ea47ca
Filip Klimes <filip@filipklimes.cz>
2015-04-06 12:38:17 +0200

    Finished

--- dist/bezpecnost/BEAUFORT.java
+++ dist/bezpecnost/BEAUFORT.java
@@ -1,4 +1,162 @@
-package cz.filipklimes;
+package bezpecnost;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+/**
+ * Beaufort cipher solver for A7B34KBE class at FEE CUT.
+ */
+class Bezpecnost {
+
+    private StringTokenizer st = new StringTokenizer("");
+    private BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+    private String type;
+    private CipherSolver cs = new BeaufortCipherSolver();
+    private Socket socket;
+    private PrintWriter printWriter;
+
+    /**
+     * Main method of the program.
+     * Runs the main loop.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        Bezpecnost inst = new Bezpecnost();
+//        try {
+//            inst.socket = new Socket(InetAddress.getByName("94.112.68.30"), 3999);
+//            inst.printWriter = new PrintWriter(inst.socket.getOutputStream(), true);
+//        } catch (IOException e) {
+//        }
+
+        while (inst.run()) {
+        }
+
+
+
+        // Close the input stream.
+        try {
+//            inst.socket.close();
+            inst.stdin.close();
+        } catch (IOException e) {
+        }
+    }
+
+    /**
+     * Main loop method of the program.
+     *
+     * @return True if you can continue to iterate, false if not.
+     */
+    boolean run() {
+        type = nextToken();
+        if (type.equals("end")) return false;
+        if (type.equals("e")) {
+            handleEncrypt();
+        }
+        if (type.equals("d")) {
+            handleDecrypt();
+        }
+
+        return true;
+    }
+
+    /**
+     * Reads next string token from the input.
+     *
+     * @return Reads the next string token from the input.
+     */
+    private String nextToken() {
+        while (!st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(stdin.readLine());
+            } catch (IOException e) {
+            }
+        }
+        return st.nextToken();
+    }
+
+
+    /**
+     * Handles the "e" command.
+     */
+    private void handleEncrypt() {
+        String key = nextToken();
+        StringBuilder inputStringBuilder = new StringBuilder();
+        while (st.hasMoreTokens()) {
+            inputStringBuilder.append(nextToken());
+        }
+
+        String output = cs.encrypt(cs.removeInvalidCharacters(key), cs.removeInvalidCharacters(inputStringBuilder.toString()));
+
+        StringBuilder outputStringBuilder = new StringBuilder();
+        int i = 0;
+        for (char c : output.toCharArray()) {
+            outputStringBuilder.append(c);
+            if(++i % 5 == 0 && i < (output.length() - 1)) {
+                outputStringBuilder.append(' ');
+            }
+        }
+        outputStringBuilder.append('\n');
+//        this.printWriter.write("e " + key + " " + inputStringBuilder.toString() + "\n");
+//        this.printWriter.write("ours: " + outputStringBuilder.toString());
+//        this.printWriter.flush();
+        System.out.print(outputStringBuilder.toString());
+    }
+
+    /**
+     * Handles the "d" command.
+     */
+    private void handleDecrypt() {
+        String key = nextToken();
+        StringBuilder sb = new StringBuilder();
+        while (st.hasMoreTokens()) {
+            sb.append(nextToken());
+        }
+//        this.printWriter.write("d " + key + " " + sb.toString() + "\n");
+//        this.printWriter.write("ours: " + cs.decrypt(cs.removeInvalidCharacters(key), cs.removeInvalidCharacters(sb.toString())));
+//        this.printWriter.flush();
+        System.out.print(cs.decrypt(cs.removeInvalidCharacters(key), cs.removeInvalidCharacters(sb.toString())) + "\n");
+    }
+}
+
+/**
+ * Encrypts and decrypts given text using a cipher algorithm.
+ *
+ * @author klimesf
+ */
+interface CipherSolver {
+
+    /**
+     * Removes invalid characters from given string.
+     *
+     * @param s String to be truncated.
+     * @return String without invalid characters.
+     */
+    String removeInvalidCharacters(String s);
+
+    /**
+     * Encrypts given text with given key.
+     *
+     * @param key  Key to be encrypted by.
+     * @param text Open text to be encrypted.
+     * @return Encrypted text.
+     */
+    String encrypt(String key, String text);
+
+    /**
+     * Decrypts given text by given key.
+     *
+     * @param key  Key to be decrypted by.
+     * @param text Encrypted text to be decrypted.
+     * @return Decrypted text.
+     */
+    String decrypt(String key, String text);
+}
 
 /**
  * Encrypts and decrypts given text using Beaufort algorithm.
@@ -6,7 +164,7 @@ package cz.filipklimes;
  * @author klimesf
  * @see "http://www.thonky.com/kryptos/beaufort-cipher/"
  */
-public class BeaufortCipherSolver implements CipherSolver {
+class BeaufortCipherSolver implements CipherSolver {
 
     private char[][] tableau;
     private char[] cols;
--- src/cz/filipklimes/Bezpecnost.java
+++ src/cz/filipklimes/Bezpecnost.java
@@ -1,6 +1,7 @@
 package cz.filipklimes;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -14,13 +15,31 @@ class Bezpecnost {
     private String type;
     private CipherSolver cs = new BeaufortCipherSolver();
 
-    public static void main(String[] args) throws Exception {
+    /**
+     * Main method of the program.
+     * Runs the main loop.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
         Bezpecnost inst = new Bezpecnost();
+
         while (inst.run()) {
+        }
+
+        // Close the input stream.
+        try {
+            inst.stdin.close();
+        } catch (IOException e) {
         }
     }
 
-    boolean run() throws Exception {
+    /**
+     * Main loop method of the program.
+     *
+     * @return True if you can continue to iterate, false if not.
+     */
+    boolean run() {
         type = nextToken();
         if (type.equals("end")) return false;
         if (type.equals("e")) {
@@ -33,28 +52,55 @@ class Bezpecnost {
         return true;
     }
 
-    private String nextToken() throws Exception {
-        while (!st.hasMoreTokens()) st = new StringTokenizer(stdin.readLine());
+    /**
+     * Reads next string token from the input.
+     *
+     * @return Reads the next string token from the input.
+     */
+    private String nextToken() {
+        while (!st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(stdin.readLine());
+            } catch (IOException e) {
+            }
+        }
         return st.nextToken();
     }
 
-    private void handleEncrypt() throws Exception {
+    /**
+     * Handles the "e" command.
+     */
+    private void handleEncrypt() {
+        String key = nextToken();
+        StringBuilder inputStringBuilder = new StringBuilder();
+        while (st.hasMoreTokens()) {
+            inputStringBuilder.append(nextToken());
+        }
+
+        String output = cs.encrypt(cs.removeInvalidCharacters(key), cs.removeInvalidCharacters(inputStringBuilder.toString()));
+
+        StringBuilder outputStringBuilder = new StringBuilder();
+        int i = 0;
+        for (char c : output.toCharArray()) {
+            outputStringBuilder.append(c);
+            if(++i % 5 == 0 && i < (output.length() - 1)) {
+                outputStringBuilder.append(' ');
+            }
+        }
+        outputStringBuilder.append('\n');
+        System.out.print(outputStringBuilder.toString());
+
+    }
+
+    /**
+     * Handles the "d" command.
+     */
+    private void handleDecrypt() {
         String key = nextToken();
         StringBuilder sb = new StringBuilder();
         while (st.hasMoreTokens()) {
             sb.append(nextToken());
         }
-        System.out.println(cs.encrypt(key, sb.toString()));
+        System.out.print(cs.decrypt(cs.removeInvalidCharacters(key), cs.removeInvalidCharacters(sb.toString())) + "\n");
     }
-
-    private void handleDecrypt() throws Exception {
-        String key = nextToken();
-        StringBuilder sb = new StringBuilder();
-        while (st.hasMoreTokens()) {
-            sb.append(nextToken());
-        }
-        System.out.println(cs.decrypt(key, sb.toString()));
-    }
-
 }
-
